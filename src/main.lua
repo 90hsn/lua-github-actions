b3223289985a31b81b350bfcf771fc8340f1a760
90hsn <goh.sn@csit.gov.sg>
2024-11-15 09:46:25 +0800

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -4,15 +4,3 @@ local output = { "Hello, reader.", 42 }
 
 print(inspect(output))
 
-local math_functions = {}
-
-function math_functions.add(a, b)
-    return a + b
-end
-
-function Test1()
-  lu.assertEquals(math_functions.add(3,5), 8)
-    print(inspect(math_functions.add(3,5)))
-end
-
-Test1()
