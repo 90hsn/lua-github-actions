3ef3756ac0ec0c77f178080b9437a7e9606b114f
90hsn <goh.sn@csit.gov.sg>
2024-11-15 10:00:09 +0800

    Update and rename math-functions-test.lua to math_functions-test.lua

--- src/examples/math_functions-test.lua
+++ src/examples/math_functions-test.lua
@@ -4,15 +4,15 @@ local math_functions = require("examples/math_functions")
 local TestMath = {}
 
 function TestMath:test1()
-	lu.assertEquals(add(1,2), 3)
+	lu.assertEquals(math_functions.add(1,2), 3)
 end
 
 function TestMath:test2()
-	lu.assertEquals(add(-65,5), -60)
+	lu.assertEquals(math_functions.add(-65,5), -60)
 end
 
 function TestMath:test3()
-	lu.assertEquals(add(4,0), 4)
+	lu.assertEquals(math_functions.add(4,0), 4)
 end
 
 return TestMath
