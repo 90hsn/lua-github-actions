5f9bf2c6b65c0dba374f0f72525bdc6a79e96493
90hsn <goh.sn@csit.gov.sg>
2024-11-15 10:17:18 +0800

    Update math_functions-test.lua

--- src/examples/math_functions-test.lua
+++ src/examples/math_functions-test.lua
@@ -1,18 +1,18 @@
 local lu = require("luaunit")
-local math_functions = require("examples/math_functions")
+local MathFunctions = require("examples/math-functions")
 
 local TestMath = {}
 
 function TestMath:test1()
-	lu.assertEquals(math_functions.add(1,2), 3)
+	lu.assertEquals(MathFunctions.add(1,2), 3)
 end
 
 function TestMath:test2()
-	lu.assertEquals(math_functions.add(-65,5), -60)
+	lu.assertEquals(MathFunctions.add(-65,5), -60)
 end
 
 function TestMath:test3()
-	lu.assertEquals(math_functions.add(4,0), 4)
+	lu.assertEquals(MathFunctions.add(4,0), 4)
 end
 
 return TestMath
