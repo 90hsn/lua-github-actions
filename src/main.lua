93fa86f43f189e7a5e9c63238ab160bce2dc5168
90hsn <goh.sn@csit.gov.sg>
2024-11-15 09:38:26 +0800

    Update main.lua
    with math

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,13 @@
 -- src/main.lua
-local greeting = "Hello, reader."
-print(greeting)
+local inspect = require "inspect"
+local output = { "Hello, reader.", 42 }
+
+print(inspect(output))
+
+local math_functions = {}
+
+function math_functions.add(a, b)
+    return a + b
+end
+
+return math_functions
